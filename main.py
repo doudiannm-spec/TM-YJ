b88a944b552dd97d6bbda120a03fd874ca0716fb
天明 <doudiannm@gmail.com>
2026-02-04 11:47:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 import re
 from datetime import datetime
@@ -9,7 +10,7 @@ from fastapi.responses import HTMLResponse
 import uvicorn
 
 # ===================== 核心配置 =====================
-BOT_TOKEN = "7310129851:AAFsjT3xpqdAurqbk_VmNE3SdJ8NO-oms9w"
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 VALID_GROUPS = ["组1", "组2", "组3", "组4"]
 DATA_PATH = "data.json"
 COMMISSION_RATE = 0.01  # 1%提成
@@ -207,4 +208,5 @@ if __name__ == "__main__":
     t2 = Thread(target=run_web)
     t1.start()
     t2.start()
+
 
